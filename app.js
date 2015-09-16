c170ed1ff692863b91ca4b16dd1fdfc4d49c8a43
Emran <emran04ch@gmail.com>
2015-09-17 00:25:56 +0600

    Got output from api

--- app.js
+++ app.js
@@ -2,24 +2,29 @@
 // pw: Wc8qNfr6pNZC
 // get call from https://api.forecast.io/forecast/278c9d582cc1c0bd6968c962eaf54bd0/22.3636,91.8033
 
+// 22.376594 91.846892
+
 // Problem: Simply get weather forecast from forcast.io by zipcode
 
 // Solution: Use node.js to get api call.
 
-var http = require('http');
+var https = require('https');
 
 function printMessage (summary, precipType, temperature) {
-	var message = "current weather condition: \nDay summary: " + summary + ",\n Type: " + precipType + "\n Temperature: " + temperature;
+	var message = " Current weather condition: \n Day summary: " + summary + ",\n Type: " + precipType + "\n Temperature: " + temperature;
 	console.log(message);
 }
 
 
-var request = http.get("http://api.forecast.io/forecast/278c9d582cc1c0bd6968c962eaf54bd0/22.3636,91.8033", function(res) {
-  console.log("Got response: " + res.statusCode);
-  res.on('data', function (chunk) {
-    console.log('BODY: ' + chunk);
+var request = https.get("https://api.forecast.io/forecast/278c9d582cc1c0bd6968c962eaf54bd0/22.376594,91.846892", function(response) {
+  var body = "";
+  //Read the data
+  response.on('data', function (chunk) {
+    body += chunk;
   });
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
+  response.on('end', function(){
+    var profile = JSON.parse(body);
+    printMessage(profile.currently.summary, profile.currently.precipType, profile.currently.temperature);
+  });
 });
 
